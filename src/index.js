392d04dbff969136e1bc61c9d01c6e8575c68c49
oladimeji <tommyslipz@gmail.com>
2023-06-13 20:06:50 +0100

    completed header and navber

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-// DEMO JOKE APP
-import generateJoke from './generateJoke'
 import './styles/main.scss'
- import laughing from './assets/laughing.svg'
+import laughing from './assets/laughing.svg'
  
- import f1 from './assets/f1.jpeg'
+import f1 from './assets/f1.jpeg';
+import logo from './assets/logo.svg';
+import caseimg from './assets/caseimg.svg'
+import layer from './assets/layer.svg'
 
 console.log('I was called again')
 // const laughImg = document.getElementById('laughImg')
 // laughImg.src = laughing
 
-const jokeBtn = document.getElementById('jokeBtn')
-jokeBtn.addEventListener('click', generateJoke)
-
-generateJoke()
