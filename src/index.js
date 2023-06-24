c97cf4997f8e06df25d5bf07370eabf0f44d3ff2
oladimeji <tommyslipz@gmail.com>
2023-06-24 15:27:52 +0100

    made responsive menu bar

--- src/index.js
+++ src/index.js
@@ -6,7 +6,22 @@ import logo from './assets/logo.svg';
 import caseimg from './assets/caseimg.svg'
 import layer from './assets/layer.svg'
 
-console.log('I was called again')
 // const laughImg = document.getElementById('laughImg')
 // laughImg.src = laughing
 
+// function showLinks(){
+//   const navLink =  document.querySelector('.mobile-nav')
+//   navLink.style.display = 'block'
+//   console.log(navLink)
+// }
+
+const menu = document.querySelector('.hamburger')
+menu.addEventListener('click', function(){
+    const navLink =  document.querySelector('.mobile-nav')
+    const html =  document.querySelector('html')
+    menu.classList.toggle('is-active')
+    navLink.classList.toggle('is-active')
+    html.classList.toggle('overflow-hidden')
+  
+})
+// console.log(menu)
